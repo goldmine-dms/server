9faa7cf59f0701607158a0820de795b6bebad36e
Christian Panton <christian@panton.org>
2012-01-09 12:37:42 +0100

    refactoring of models.auth.__init__.py

--- goldmine/models/auth/__init__.py
+++ goldmine/models/auth/__init__.py
@@ -3,5 +3,7 @@
 
 from goldmine.models.auth.token import Token
 from goldmine.models.auth.user import User
+from goldmine.models.auth.group import Group, GroupMember
+from goldmine.models.auth.permission import Permission
 
 
